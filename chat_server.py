7845e6ae9ecc7c9dc72b6238571597fd3a7c74eb
minekoa <mineko.orange@gmail.com>
2016-05-28 02:18:13 +0900

    ChatApp をクラス化して整理

--- chat_server.py
+++ chat_server.py
@@ -5,64 +5,70 @@ import cgi
 import wsgiref.util
 from chat_commander import ChatCommander
 
-def broadcast(msg):
-    remove = set()
-    for s in ws_list:
-        try:
-            s.send(msg)
-        except Exception:
-            remove.add(s)
-    for s in remove:
-        ws_list.remove(s)
-
-ws_list   = set()
-commander = ChatCommander()
-commander.set_message_function(broadcast)
-
-def chat_handle(environ, start_response):
-    ws = environ['wsgi.websocket']
-    ws_list.add(ws)
-    print 'enter!', len(ws_list)
-    while 1:
-        msg = ws.receive()
-        if msg is None: break
-
-        broadcast(msg)
-        commander.run(msg)
-        for cmsg in commander.messages:
-            broadcast(cmsg)
-
-    print 'exit!', len(ws_list)
-
 def wsgi_query(environ):
     wsgi_input     = environ['wsgi.input']
     content_length = int(environ.get('CONTENT_LENGTH'))
     return dict(cgi.parse_qsl(wsgi_input.read(content_length)))
 
-def chatapp(environ, start_response):  
-    path    = environ["PATH_INFO"]
-    method  = environ["REQUEST_METHOD"]
-    app_uri = wsgiref.util.application_uri(environ).strip('/').split('://')   # "<scheme>://<netloc>/" -> ["<scheme>", "<netloc>"]
+def wsgi_scheme_and_netloc(environ):
+    ''' "<scheme>://<netloc>/" -> ["<scheme>", "<netloc>"] '''
+    return wsgiref.util.application_uri(environ).strip('/').split('://')   
 
-    if path == "/": 
+class ChatApp(object):
+    def __init__(self):
+        self.ws_list   = set()
+        self.commander = ChatCommander()
+        self.commander.set_message_function(self.broadcast)
+
+    def broadcast(self, msg):
+        remove = set()
+        for ws in self.ws_list:
+            try:
+                ws.send(msg)
+            except Exception:
+                remove.add(ws)
+        for ws in remove:
+            self.ws_list.remove(ws)
+
+    def __call__(self, environ, start_response):  
+        path    = environ["PATH_INFO"]
+        method  = environ["REQUEST_METHOD"]
+     
+        if path == "/": 
+            return self.index_handle(environ, start_response)
+        elif path == "/client" and method == "POST":
+            return self.client_handle(environ, start_response)
+        elif path == "/chat":  
+            return self.chat_handle(environ, start_response)
+     
+        else:
+            start_response("404 NOT FOUND", [("Content-Type", "text/plain")])  
+            return "404 NOT FOUND ;-p"
+
+    def index_handle(self, environ, start_response):
         start_response("200 OK", [("Content-Type", "text/html;charset=UTF-8")])  
-        uri = "%s://%s/client" % (app_uri[0], app_uri[1])
+        schme_netloc = wsgi_scheme_and_netloc(environ)
+        uri = "%s://%s/client" % (schme_netloc[0], schme_netloc[1])
         return open('./chat_login.html').read() % uri
 
-    elif path == "/client" and method == "POST":
+    def client_handle(self, environ, start_response):
         query = wsgi_query(environ)
         username = query['name']
         start_response("200 OK", [("Content-Type", "text/html;charset=UTF-8")])  
-        uri = "ws://%s/chat" % app_uri[1]
+        uri = "ws://%s/chat" % wsgi_scheme_and_netloc(environ)[1]
         return open('./chat_client.html').read() % (uri, username)
 
-    elif path == "/chat":  
-        return chat_handle(environ, start_response)
-
-    else:
-        start_response("404 NOT FOUND", [("Content-Type", "text/plain")])  
-        return "404 NOT FOUND ;-p"
+    def chat_handle(self, environ, start_response):
+        ws = environ['wsgi.websocket']
+        self.ws_list.add(ws)
+        print 'enter!', len(self.ws_list)
+        while 1:
+            msg = ws.receive()
+            if msg is None: break
+            self.broadcast(msg)
+            self.commander.run(msg)
+        print 'exit!', len(ws_list)
 
 if __name__ == '__main__':
-    server = pywsgi.WSGIServer(('0.0.0.0', 8080), chatapp, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', 8080), ChatApp(), handler_class=WebSocketHandler)
     server.serve_forever()
